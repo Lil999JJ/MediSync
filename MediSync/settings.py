633dab32c39a3949f9cf758593d8321abe4982c4
Enyer <20230347@miucateci.edu.do>
2025-07-03 10:30:52 -0400

    Posgrest

--- MediSync/settings.py
+++ MediSync/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'MediSync.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'citmed',
+        'USER': 'postgres',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
 
